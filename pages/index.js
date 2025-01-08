959412af35c5fd7715c80285050698d76ab9acff
Olga Novikova <olganovikovaborges@gmail.com>
2025-01-08 18:35:57 -0300

    adds scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Home() {
-    return (
-      <pre>
-        Estudar programação é muito divertido
-      </pre>
-    );
-  }
-  
-  export default Home;
+  return <pre>Estudar programação é muito divertido</pre>;
+}
+
+export default Home;
+
+function teste3() {
+  console.log("identação errada");
+}
